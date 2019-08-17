ceb74003190085f0c353d7780e52f5dbd12ef7c8
Ludwig Simon <ludwigsimon77@gmail.com>
2019-08-17 12:10:57 +0200

    Use new domain name for tvtime

--- see_them_all/util/constants.py
+++ see_them_all/util/constants.py
@@ -4,7 +4,7 @@ from event_bus import EventBus
 
 TVTIME_CLIENT_ID = 'UOWED7wBGRQv17skSZJO'
 TVTIME_CLIENT_SECRET = 'ZHYcO8n8h6WbYuMDWVgXr7T571ZF_s1r1Rzu1-3B'
-TVTIME_BASEURL = 'https://api.tvshowtime.com'
+TVTIME_BASEURL = 'https://api.tvtime.com'
 TVTIME_DEVICE_CODE = urljoin(TVTIME_BASEURL, '/v1/oauth/device/code')
 TVTIME_TOKEN = urljoin(TVTIME_BASEURL, '/v1/oauth/access_token')
 TVTIME_CHECKIN = urljoin(TVTIME_BASEURL, '/v1/checkin')
